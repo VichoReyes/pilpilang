c9e16801c97206047010ad66fb803897dc762a07
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-08-18 17:35:08 -0400

    add test utilities

--- test/test-foo.hs
+++ test/test-foo.hs
@@ -3,10 +3,23 @@
 module Main (main) where
 
 import System.Exit (exitFailure)
-import Text.Megaparsec (runParser)
+import Text.Megaparsec (runParser, parseTest, eof)
+import Data.Either (isLeft)
 import Types (Parser, parseBool)
+import qualified Data.Text as T
 
-main = do
-    if runParser parseBool "" "true" == Right True
+parsePass :: (Eq a) => Parser a -> T.Text -> a -> IO ()
+parsePass parser text result = do
+    if runParser (parser <* eof) "" text == Right result
       then return ()
       else exitFailure
+
+parseFail :: Parser a -> T.Text -> IO ()
+parseFail parser text = do
+    if isLeft $ runParser (parser <* eof) "" text
+      then return ()
+      else exitFailure
+
+main = do
+    parsePass parseBool "true" True
+    parseFail parseBool "asdfasdf"
