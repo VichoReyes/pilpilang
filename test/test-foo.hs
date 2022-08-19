7d1c0a8e636d913a34d8c84d9347300b2d9ab062
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-08-19 13:09:51 -0400

    remove old stuff

--- test/test-foo.hs
+++ test/test-foo.hs
@@ -3,23 +3,25 @@
 module Main (main) where
 
 import System.Exit (exitFailure)
-import Text.Megaparsec (runParser, parseTest, eof)
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MP.C
 import Data.Either (isLeft)
-import Types (Parser, parseBool)
+import Types (Parser)
 import qualified Data.Text as T
 
 parsePass :: (Eq a) => Parser a -> T.Text -> a -> IO ()
 parsePass parser text result = do
-    if runParser (parser <* eof) "" text == Right result
+    if MP.runParser (parser <* MP.eof) "" text == Right result
       then return ()
       else exitFailure
 
 parseFail :: Parser a -> T.Text -> IO ()
 parseFail parser text = do
-    if isLeft $ runParser (parser <* eof) "" text
+    if isLeft $ MP.runParser (parser <* MP.eof) "" text
       then return ()
       else exitFailure
 
 main = do
-    parsePass parseBool "true" True
-    parseFail parseBool "asdfasdf"
+    parsePass (MP.C.char 'a') "a" 'a'
+    parsePass (MP.C.string' "hola") "HoLa" "HoLa"
+    parseFail (MP.C.string' "hola") "HoLaa"
