d585bd6c186bb7993310cdabe661dfeb685aa30e
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-01 12:48:42 -0400

    add parsing files to executable

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,20 @@ module Main where
 
 import Syntax
 import Text.Megaparsec
+import System.Environment (getArgs)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
 main = do
-    putStrLn "Hello Pilpilang!"
-    contents <- T.pack <$> getContents
-    parseTest (pAssoc <* eof) contents
+    args <- getArgs
+    if (null args)
+    then do
+        putStrLn "Hello Pilpilang!"
+        contents <- T.pack <$> getContents
+        parseTest (pAssoc <* eof) contents
+    else do
+        case args of
+            ["AST", path] -> TIO.readFile path >>= parseTest (pAST <* eof)
+            ["assoc", path] -> TIO.readFile path >>= parseTest (pAssoc <* eof)
+            _ -> putStrLn "bad usage"
