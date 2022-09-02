65de86736ca86f1cfba215fb72171e6279972344
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-02 12:36:19 -0400

    restructure test and use hspec-megaparsec

---
+++ test/SyntaxSpec.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module SyntaxSpec (main, spec) where
+
+import Text.Megaparsec
+import Test.Hspec
+import Test.Hspec.Megaparsec
+import Data.Char (ord)
+import Syntax
+import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO
+
+main :: IO ()
+main = hspec spec
+
+
+data MockDB
+
+instance ColumnTypeProvider MockDB where
+    fillTypes _ = fillColumnTypes
+
+-- very much mock
+fillColumnTypes :: GEntity a Text -> IO (GEntity a (Text, Text))
+fillColumnTypes (Entity name table cols) = do
+    return $ Entity name table typedCols
+        where
+            typedCols = do
+                untypedCol <- cols
+                let colType = case T.uncons untypedCol of
+                                   Just (c, _) -> typeFrom c
+                                   Nothing -> undefined
+                return (untypedCol, colType)
+            typeFrom c = case (ord c `mod` 3) of
+                0 -> "Int"
+                1 -> "Bool"
+                2 -> "String"
+                _ -> undefined
+
+
+actor1 :: Actor
+actor1 = actor "MyActor" "actors" ["col1", "col2"]
+
+actor1_desc :: Text
+actor1_desc = "actor MyActor {\
+\    table \"actors\"\
+\    columns [\"col1\", \"col2\"] \
+\ }"
+
+spec :: Spec
+spec = do
+    describe "pActor" $ do
+        it "requires whitespace after symbol" $
+            parse pActor "" `shouldFailOn` "actorActor { table \"tablename\" columns []}"
+        it "parses correct descriptions" $ do
+            parse pActor "" `shouldSucceedOn` "actor Actor { table \"tablename\" columns []}"
+            parse pActor "" actor1_desc `shouldParse` actor1
+        it "parses an actor without columns" $
+            parse pActor "" "actor Actor { table \"tablename\" }" `shouldParse`
+                (actor "Actor" "tablename" [])
+        it "handles underscores" $
+            parse pActor "" `shouldSucceedOn` "actor Actor_1 {table \"asdf\" }"
+        it "disallows empty table names" $
+            parse pActor "" `shouldFailOn` "actor Ac { table \"\" }"
+    
+    describe "pResource" $
+        it "works" $
+            parse pResource "" `shouldSucceedOn` "resource Re {table \"asdf\"}"
+
+    describe "pQuotedLiteral" $
+        it "handles escaped quotes" $
+            parse (pQuotedLiteral True) "" "\"escapedquote\\\"here\"" `shouldParse` "escapedquote\"here"
+
+    describe "pAssoc" $ do
+        it "works" $
+            parse pAssoc "" "can_write(actor: Actor, resource: Doc) if 7 = actor.age" `shouldParse`
+                Assoc { 
+                    assocHeader = AHPermission (
+                        Permission {
+                            permissionType = PCanWrite, 
+                            permissionActor = TypedVar {typedVarName = "actor", typedVarType = "Actor"},
+                            permissionResource = TypedVar {typedVarName = "resource", typedVarType = "Doc"}
+                            }
+                        ), 
+                    assocDefinition = PEquals (VLitInt 7) (VVarField "actor" "age")}
+        it "handles AND predicates (&&)" $
+            parse pAssoc "" "can_write(actor: T, resource: Doc) if hola(asdfasd) && \"cinco\" = 5" `parseSatisfies`
+                (\assoc -> case assocDefinition assoc of
+                    (PAnd (PCall _) (PEquals _ _)) -> True
+                    _ -> False)
+        it "handles OR predicates (||)" $
+            parse pAssoc "" "numbers(a: A, r: T) if a >4   || (r   <8   && a  =  r)" `parseSatisfies`
+                (\assoc -> case assocDefinition assoc of
+                    (POr (PGreaterT _ _) (PAnd (PLessT _ _) (PEquals _ _))) -> True
+                    _ -> False)
+    
+    describe "pAST" $
+        it "parses the twitter example" $
+            TIO.readFile "test/examples/twitter.pilpil" >>= (parse pAST "" `shouldSucceedOn`)
