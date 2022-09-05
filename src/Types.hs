d1769df6d56db4e53624dbe28780cd63edfe9e3a
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-05 14:47:55 -0400

    typecheck predcalls

--- src/Types.hs
+++ src/Types.hs
@@ -7,7 +7,7 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Control.Monad.State (StateT, MonadState (put, get), MonadTrans (lift), execStateT)
 import Control.Monad (forM_, forM, when)
-import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (local), asks)
+import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (local, ask))
 
 class ColumnTypeProvider a where
     fillTypes :: a -> GEntity klass Text -> Either TypeError (GEntity klass (Text, Text))
@@ -86,6 +86,13 @@ mkGlobals provider ast = do
 cTypeError :: Text -> TypeCheck a
 cTypeError err = lift (Left (TypeError err))
 
+cLookUp :: (TypeInfo -> Maybe a) -> Text -> TypeCheck a
+cLookUp f err = do
+    typeInfo <- ask
+    case f typeInfo of
+        Nothing -> cTypeError err
+        Just a -> return a
+
 cAssocs :: [Assoc] -> TypeCheck ()
 cAssocs associations = do
     forM_ associations $ \assoc -> do
@@ -102,11 +109,10 @@ mkLocalVars (AHPermission (Permission _ actorVar resourceVar)) = do
     let (resourceName, resourceType) = var2pair resourceVar
     when (actorName == resourceName) $
         cTypeError ("actor "<>actorName<>" and resource "<>resourceName<>" can't have the same name")
-    ents <- asks entities
-    (actorClass, _) <- maybe (cTypeError $ actorName<>" not defined") (lift . Right) (M.lookup actorName ents)
+    (actorClass, _) <- cLookUp (M.lookup actorName . entities) ("Actor "<>actorName<>" not defined")
     when (actorClass /= EActor) $
         cTypeError "when defining a permission, first argument should be an Actor"
-    (resourceClass, _) <- maybe (cTypeError $ resourceName<>" not defined") (lift . Right) (M.lookup resourceName ents)
+    (resourceClass, _) <- cLookUp (M.lookup resourceName . entities) ("Resource "<>resourceName<>" not defined")
     when (resourceClass /= EResource) $
         cTypeError "when defining a permission, second argument should be an Resource"
     return mempty {variables = M.fromList [(actorName, actorType), (resourceName, resourceType)]}
@@ -115,7 +121,27 @@ var2pair :: TypedVar -> (Text, Type)
 var2pair var = (typedVarName var, typedVarType var)
 
 cPredicate :: Predicate -> TypeCheck ()
-cPredicate (PCall _) = undefined
+cPredicate (PCall (PredCall predName args)) = do
+    expectedTypes <- cLookUp (M.lookup predName . functions) $ 
+        (predName<>" not defined")
+    actualTypes <- forM args cValue
+    when (length expectedTypes /= length actualTypes) $
+        cTypeError (predName<>" call with wrong number of arguments")
+    forM_ (zip expectedTypes actualTypes) $ \(exp, act) ->
+        when (exp /= act)
+            (cTypeError $ "in call to "<>predName<>": expected "<>exp<>", found "<>act)
+
+cValue :: Value -> TypeCheck Type
+cValue (VLitInt _) = return "Int"
+cValue (VLitBool _) = return "Bool"
+cValue (VLitString _) = return "String"
+cValue (VVar varName) = cLookUp (M.lookup varName . variables) (varName<>" not found")
+cValue (VVarField varName varField) = cLookUp lookUpField (varName<>"."<>varField<>" not found")
+    where
+        lookUpField typeInfo = do
+            varType <- M.lookup varName (variables typeInfo)
+            fields <- M.lookup varType (entities typeInfo)
+            M.lookup varField (snd fields)
 
 addFunction :: Text -> [TypedVar] -> TypeGen ()
 addFunction name args = do
