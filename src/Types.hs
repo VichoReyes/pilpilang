0efd6ed1b33bd6c6cd6434541fb5176aea080f28
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-05 13:02:50 -0400

    implement mkLocalVars

--- src/Types.hs
+++ src/Types.hs
@@ -6,8 +6,8 @@ import Data.Text (Text)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Control.Monad.State (StateT, MonadState (put, get), MonadTrans (lift), execStateT)
-import Control.Monad (forM_, forM)
-import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (local))
+import Control.Monad (forM_, forM, when)
+import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (local), asks)
 
 class ColumnTypeProvider a where
     fillTypes :: a -> GEntity klass Text -> Either TypeError (GEntity klass (Text, Text))
@@ -83,6 +83,9 @@ mkGlobals provider ast = do
             AHDef (Definition name args) -> addFunction name args
             _ -> return () -- skip permissions, they always have the same types
 
+cTypeError :: Text -> TypeCheck a
+cTypeError err = lift (Left (TypeError err))
+
 cAssocs :: [Assoc] -> TypeCheck ()
 cAssocs associations = do
     forM_ associations $ \assoc -> do
@@ -91,11 +94,22 @@ cAssocs associations = do
         local (localVars<>) (cPredicate predicate)
 
 mkLocalVars :: AssocHeader -> TypeCheck TypeInfo
-mkLocalVars (AHDef (Definition _ vars)) = 
+mkLocalVars (AHDef (Definition _ vars)) =
     let varsMap = M.fromList $ map var2pair vars
     in return mempty {variables = varsMap}
 mkLocalVars (AHPermission (Permission _ actorVar resourceVar)) = do
-    undefined -- I'm going to need the types
+    let (actorName, actorType) = var2pair actorVar
+    let (resourceName, resourceType) = var2pair resourceVar
+    when (actorName == resourceName) $
+        cTypeError ("actor "<>actorName<>" and resource "<>resourceName<>" can't have the same name")
+    ents <- asks entities
+    (actorClass, _) <- maybe (cTypeError $ actorName<>" not defined") (lift . Right) (M.lookup actorName ents)
+    when (actorClass /= EActor) $
+        cTypeError "when defining a permission, first argument should be an Actor"
+    (resourceClass, _) <- maybe (cTypeError $ resourceName<>" not defined") (lift . Right) (M.lookup resourceName ents)
+    when (resourceClass /= EResource) $
+        cTypeError "when defining a permission, second argument should be an Resource"
+    return mempty {variables = M.fromList [(actorName, actorType), (resourceName, resourceType)]}
 
 var2pair :: TypedVar -> (Text, Type)
 var2pair var = (typedVarName var, typedVarType var)
