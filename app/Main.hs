18fb33a171e16452a6d8278a182cb5aa079ed141
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-09-28 22:04:44 -0300

    improve Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,25 @@ import Text.Megaparsec
 import System.Environment (getArgs)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Types (runTypeChecker, TypeError (TypeError), ColumnTypeProvider)
+
+data DBThing = DBThing
+
+instance ColumnTypeProvider DBThing
 
 main :: IO ()
 main = do
     args <- getArgs
-    if null args
-    then do
-        putStrLn "Hello Pilpilang!"
-        contents <- T.pack <$> getContents
-        parseTest (pAssoc <* eof) contents
-    else do
-        case args of
-            ["AST", path] -> TIO.readFile path >>= parseTest (pAST <* eof)
-            ["assoc", path] -> TIO.readFile path >>= parseTest (pAssoc <* eof)
-            _ -> putStrLn "bad usage"
+    case args of
+        [] -> do
+            putStrLn "Hello Pilpilang!"
+            contents <- T.pack <$> getContents
+            parseTest (pAssoc <* eof) contents
+        [path] -> do
+            contents <- TIO.readFile path
+            parseTest (pAST <* eof) contents
+            let Right ast = parse (pAST <* eof) "" contents
+            case runTypeChecker DBThing ast of
+                Right _ -> putStrLn "type checked correctly"
+                Left (TypeError t) -> TIO.putStrLn t
+        _ -> putStr "invalid"
